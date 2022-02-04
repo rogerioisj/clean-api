cb7f80659cf9f682e5a6a7d0b591dbbff9bff312
Roger Inacio <rogerio.reaper@gmail.com>
2022-02-04 18:25:30 -0300

    feat: implements a new interface to controller

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missing-param.error'
 import { badRequest } from '../helpers/http.helper'
+import { Controller } from '../protocols/controller.protocol'
 import { HttpRequest, HttpResponse } from '../protocols/http.protocol'
 
-export class SignupController {
+export class SignupController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
