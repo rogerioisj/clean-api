28265cb85120f05def07e2df076a7c27deeb74ed
Roger Inacio <rogerio.reaper@gmail.com>
2022-02-04 16:34:20 -0300

    test: ensure error 400 when email param is null

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,20 @@ describe('Signup Controle', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'Any Name',
+        password: 'any_password',
+        passWordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
   })
 })
