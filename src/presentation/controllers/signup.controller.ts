1f080abfd7e520fd6401ae0e2a8b2e9e63a20a7f
Roger Inacio <rogerio.reaper@gmail.com>
2022-02-04 16:33:59 -0300

    feat: ensure error 400 when email param is null

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -2,13 +2,15 @@ export class SignupController {
   handle (httpRequest: any): any {
     if (!httpRequest.body.name) {
       return {
-        statusCode: 400
+        statusCode: 400,
+        body: new Error('missing param: name')
       }
     }
 
     if (!httpRequest.body.email) {
       return {
-        statusCode: 400
+        statusCode: 400,
+        body: new Error('missing param: email')
       }
     }
   }
