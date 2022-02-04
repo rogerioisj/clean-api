0850327744474fb6bb616aeaca8a28db707ffdb2
Roger Inacio <rogerio.reaper@gmail.com>
2022-02-04 18:33:30 -0300

    feat: set validation for email

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from '../errors/invalid-param.error'
 import { MissingParamError } from '../errors/missing-param.error'
 import { badRequest } from '../helpers/http.helper'
 import { Controller } from '../protocols/controller.protocol'
+import { EmailValidator } from '../protocols/email-validator.protocol'
 import { HttpRequest, HttpResponse } from '../protocols/http.protocol'
 
 export class SignupController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -11,6 +19,12 @@ export class SignupController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return {
