e21b948d99f3b76ed1982f165bbaa0c1aa68647e
Roger Inacio <rogerio.reaper@gmail.com>
2022-02-04 16:41:19 -0300

    test: ensure 400 when password confirm is missing

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -41,4 +41,18 @@ describe('Signup Controle', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('missing param: password'))
   })
+
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'Any Name',
+        email: 'any_email@test.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: passWordConfirmation'))
+  })
 })
